433b77c8d2f5454aafed9d31a7d1f4aaca608691
KrazyFunkyDude <160782829+KrazyFunkyDude@users.noreply.github.com>
2024-09-04 10:38:25 +0600

    Initial commit for new changes

---
+++ app/components/Toggle.js
@@ -0,0 +1,8 @@
+"use client";
+import react from "react"
+
+const buttonToggle = () => {
+
+};
+
+export default buttonToggle;
---
+++ app/users/page.js
@@ -0,0 +1,9 @@
+import next from 'next'
+
+const UsersPage = () => {
+  return (
+    <div>UsersPage</div>
+  )
+}
+
+export default UsersPage
